5dc8f2b095510ad2574379fcbb6ce7da829ff085
Alex Kulemin <kulemin.dev@gmail.com>
2025-02-13 16:30:34 +0300

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import Bots from './components/Bots/Bots';
 import Header from './components/Header/Header';
 import MainInfo from './components/MainInfo/MainInfo';
@@ -5,26 +7,28 @@ import Navigation from './components/Navigation/Navigation';
 import data from './data/data.min.json';
 import { DataDto } from './models/data-dto';
 import styles from './styles.module.css';
-
 function App() {
 	const { trading_capital, trading_capital_currency, balance, on_hold, bots } =
 		data as DataDto;
 
 	return (
-		<div className={styles.app}>
-			<div className={styles.container}>
-				<Header />
-				<MainInfo
-					capital={trading_capital}
-					currency={trading_capital_currency}
-					balance={balance}
-					hold={on_hold}
-				/>
+		<>
+			<div className={styles.app}>
+				<div className={styles.container}>
+					<Header />
+					<MainInfo
+						capital={trading_capital}
+						currency={trading_capital_currency}
+						balance={balance}
+						hold={on_hold}
+					/>
 
-				<Bots items={bots} />
-				<Navigation />
+					<Bots items={bots} />
+					<Navigation />
+				</div>
 			</div>
-		</div>
+			<Analytics />
+		</>
 	);
 }
 
